2de3fc2fa5c2eb2a79e021a7336f707c19e53be2
QaisAlsaid <qaisalsaid4@gmail.com>
2026-09-17 23:54:48 +0300

    better workflow.

--- package.nix
+++ package.nix
@@ -4,16 +4,15 @@
   fetchurl,
   dpkg,
   autoPatchelfHook,
-  wrapGAppsHook3,
+  copyDesktopItems,
+  makeDesktopItem,
   udev,
   libdrm,
   libpqxx,
   unixODBC,
+  libpulseaudio,
   gst_all_1,
   xorg,
-  libpulseaudio,
-  makeDesktopItem,
-  copyDesktopItems,
 }:
 
 let
@@ -21,20 +20,22 @@ let
 in
 stdenv.mkDerivation {
   pname = "freedownloadmanager";
-  
   inherit (release) version;
 
   src = fetchurl {
-    url = "https://files2.freedownloadmanager.org/6/latest/freedownloadmanager.deb";
-    inherit (release) hash;
+    inherit (release) url hash;
   };
 
-  unpackPhase = "dpkg-deb -x $src .";
+  unpackPhase = ''
+    runHook preUnpack
+    dpkg-deb -x $src .
+    chmod -R u+w .
+    runHook postUnpack
+  '';
 
   nativeBuildInputs = [
     dpkg
     autoPatchelfHook
-    wrapGAppsHook3
     copyDesktopItems
   ];
 
@@ -45,14 +46,18 @@ stdenv.mkDerivation {
     stdenv.cc.cc
     udev
     libpulseaudio
-  ] ++ (with gst_all_1; [
+  ]
+  ++ (with gst_all_1; [
     gstreamer
     gst-libav
     gst-plugins-base
     gst-plugins-good
     gst-plugins-bad
     gst-plugins-ugly
-  ]) ++ (with xorg; [
+  ])
+  ++ (with xorg; [
+    libX11
+    libxcb
     xcbutilwm
     xcbutilimage
     xcbutilkeysyms
@@ -65,21 +70,28 @@ stdenv.mkDerivation {
       exec = "freedownloadmanager";
       icon = "freedownloadmanager";
       desktopName = "Free Download Manager";
-      categories = [ "Network" "FileTransfer" ];
+      categories = [
+        "Network"
+        "FileTransfer"
+      ];
     })
   ];
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/opt/freedownloadmanager
-    cp -r opt/freedownloadmanager/* $out/opt/freedownloadmanager/
+    cp -r opt/freedownloadmanager/. $out/opt/freedownloadmanager/
 
     mkdir -p $out/bin
     ln -s $out/opt/freedownloadmanager/fdm $out/bin/freedownloadmanager
 
-    mkdir -p $out/share/pixmaps
     if [ -f "$out/opt/freedownloadmanager/icon.png" ]; then
-      cp "$out/opt/freedownloadmanager/icon.png" $out/share/pixmaps/freedownloadmanager.png
+      install -Dm644 "$out/opt/freedownloadmanager/icon.png" \
+        "$out/share/pixmaps/freedownloadmanager.png"
     fi
+
+    runHook postInstall
   '';
 
   meta = with lib; {
--- version.nix
+++ version.nix
@@ -1,4 +1,5 @@
 {
   version = "6.34";
+  url = "https://github.com/QaisAlsaid/fdm-nix/releases/download/v6.34.0.6149/freedownloadmanager.deb";
   hash = "sha256-KZxb7xgLV4riI+A6EIJ5w7gOx/m84+F5JGnUbe4vxs0=";
 }
